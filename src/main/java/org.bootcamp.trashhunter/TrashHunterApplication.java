44ad90cfd435387f6bdb9e966ef63686d2ef2aad
Matvey Kukharchuk <bull3003@gmail.com>
2019-08-18 20:04:26 +0200

    start

---
+++ src/main/java/org.bootcamp.trashhunter/TrashHunterApplication.java
@@ -0,0 +1,13 @@
+package org.bootcamp.trashhunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrashHunterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrashHunterApplication.class, args);
+    }
+
+}
