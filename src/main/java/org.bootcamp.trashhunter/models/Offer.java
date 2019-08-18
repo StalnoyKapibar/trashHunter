55f1bd197252c1085ff51f16bf7084f0b952a10a
ZuiQuanZ <123casper@mail.ru>
2019-08-18 21:32:01 +0300

    запускается

--- src/main/java/org.bootcamp.trashhunter/models/Offer.java
+++ src/main/java/org.bootcamp.trashhunter/models/Offer.java
@@ -1,7 +1,17 @@
 package org.bootcamp.trashhunter.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Offer {
+
+    @Id
+    @GeneratedValue
+    public Long id;
+
+    @Column(name = "name", unique = true)
+    private String name;
 }
