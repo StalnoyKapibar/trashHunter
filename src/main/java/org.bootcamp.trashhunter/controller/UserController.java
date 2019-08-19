5649ece517a4f356452fe5bfa39e15e731aebb4d
freegastello <freegastello@gmail.com>
2019-08-19 15:10:29 +0300

    loginpage isWorking-2

---
+++ src/main/java/org.bootcamp.trashhunter/controller/UserController.java
@@ -0,0 +1,13 @@
+package org.bootcamp.trashhunter.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+	@RequestMapping(value = {"/", "/login"}, method = RequestMethod.GET)
+	public ModelAndView start() {
+		return new ModelAndView("login");
+	}
+}
---
+++ src/main/java/org.bootcamp.trashhunter/controller/UserRestController.java
@@ -0,0 +1,25 @@
+package org.bootcamp.trashhunter.controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/")
+public class UserRestController {
+
+	@PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity getUser(@RequestParam String loginUser,
+								  @RequestParam String passUser) {
+
+		if (loginUser == null || passUser == null) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+
+//		if (userAuth == null) {
+//			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//		}
+
+		return new ResponseEntity(loginUser, HttpStatus.OK);
+	}
+}
