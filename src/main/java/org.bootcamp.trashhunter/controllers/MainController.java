9d606f2a3c83b99bc6e6103f32c1c4fc0f60db8b
ZuiQuanZ <123casper@mail.ru>
2019-08-19 23:57:40 +0300

    thymleaf sec ready

--- src/main/java/org.bootcamp.trashhunter/controllers/MainController.java
+++ src/main/java/org.bootcamp.trashhunter/controllers/MainController.java
@@ -1,5 +1,14 @@
 package org.bootcamp.trashhunter.controllers;
 
+import org.springframework.security.access.expression.SecurityExpressionRoot;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,25 +17,41 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
+import java.util.Set;
 
 @Controller
 public class MainController {
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public ModelAndView login(Model model, String error, String logout, Principal user) {
         ModelAndView mv = new ModelAndView("/index");
-        if(user != null) {
+        if (user != null) {
             return mv;
         }
         return new ModelAndView("/login");
     }
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public ModelAndView index(Model model, String error, String logout, Principal user) {
-        ModelAndView mv = new ModelAndView("/index");
-        return mv;
+    public ModelAndView index(Model model, String error, String logout, Authentication authentication, SecurityContextHolderAwareRequestWrapper request) {
+        boolean b = request.isUserInRole("Sender");
+        System.out.println(b);
+        Set<String> authorities = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+        if (authentication.isAuthenticated()) {
+            if (authorities.contains("Sender")) {
+                ModelAndView mv = new ModelAndView("sender/sender_page");
+                return mv;
+            } else {
+                ModelAndView mv = new ModelAndView("taker/taker_page");
+                return mv;
+            }
+        } else {
+            ModelAndView mv = new ModelAndView("index");
+            return mv;
+        }
     }
 
 
     @GetMapping("/sender_my_offers")
-    public String senderMyOffers() { return "sender/sender_my_offers";}
+    public String senderMyOffers() {
+        return "sender/sender_my_offers";
+    }
 }
