46e03af49593620d8403403b79a46759cb701d3f
johnlivingstonseagul <johnlivingstonseagul@yandex.ru>
2019-08-19 11:34:52 +0400

    hey

--- src/main/java/org.bootcamp.trashhunter/TrashHunterApplication.java
+++ src/main/java/org.bootcamp.trashhunter/TrashHunterApplication.java
@@ -16,7 +16,7 @@ public class TrashHunterApplication {
     }
 
     //нужно раскоментить @Bean для создания таблиц
-    //@Bean(initMethod = "init")
+    @Bean(initMethod = "init")
     @PostConstruct
     public InitData initData() {
         return new InitData();
