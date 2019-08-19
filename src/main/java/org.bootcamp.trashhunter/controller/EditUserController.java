3bb5f376fdc0157b74bab1dfe0f94848fd5ce516
alls23 <alls232323@gmail.com>
2019-08-19 11:36:58 +0200

    edit page

--- src/main/java/org.bootcamp.trashhunter/controller/EditUserController.java
+++ src/main/java/org.bootcamp.trashhunter/controller/EditUserController.java
@@ -1,5 +1,9 @@
 package org.bootcamp.trashhunter.controller;
 
+import org.bootcamp.trashhunter.models.Sender;
+import org.bootcamp.trashhunter.models.Taker;
+import org.bootcamp.trashhunter.models.User;
+import org.bootcamp.trashhunter.services.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +14,21 @@ import java.security.Principal;
 public class EditUserController  {
 
     @Autowired
+    UserService userService;
 
     @GetMapping(value = "/edit")
     public ModelAndView index(Model model, String error, String logout, Principal user) {
-        ModelAndView mv = new ModelAndView("index");
+        String email = user.getName();
+        User user1 = userService.findByEmail(email);
+        ModelAndView mv = new ModelAndView("edit");
+
+        if( user1 != null && user1.getClass() == Sender.class) {
+            Sender sender = (Sender) user1;
+            model.addAttribute("user", sender);
+        } else if (user1 != null && user1.getClass() == Taker.class) {
+            Taker taker = (Taker) user1;
+            mv.addObject("user", taker);
+        }
         return mv;
     }
 }
