2a95f311ffd9f56fe2a070d5b24cf49a9e6b9289
ZuiQuanZ <123casper@mail.ru>
2019-08-20 18:20:42 +0300

    add isActive to offer

--- src/main/resources/static/js/sender/sender_offers.js
+++
@@ -1,9 +0,0 @@
-function confirmOffer(id) {
-    $.ajax({
-        url: '/sender/confirmOffer/' + id,
-        type: 'GET',
-        success: function () {
-            location.href = '/sender_my_offers'
-        }
-    });
-}
