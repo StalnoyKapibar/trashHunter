5c52a280aa3f753265f447f54a248282171f2f31
ZuiQuanZ <123casper@mail.ru>
2019-08-20 11:15:54 +0300

    шапка страницы оферов сендера + распределение по страницам в зависимости от auth

--- src/main/java/org.bootcamp.trashhunter/controllers/MainController.java
+++ src/main/java/org.bootcamp.trashhunter/controllers/MainController.java
@@ -1,14 +1,6 @@
 package org.bootcamp.trashhunter.controllers;
 
-import org.springframework.security.access.expression.SecurityExpressionRoot;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,12 +9,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
-import java.util.Set;
 
 @Controller
 public class MainController {
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public ModelAndView login(Model model, String error, String logout, Principal user) {
+
         ModelAndView mv = new ModelAndView("/index");
         if (user != null) {
             return mv;
@@ -30,13 +22,13 @@ public class MainController {
         return new ModelAndView("/login");
     }
 
+
     @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public ModelAndView index(Model model, String error, String logout, Authentication authentication, SecurityContextHolderAwareRequestWrapper request) {
-        boolean b = request.isUserInRole("Sender");
-        System.out.println(b);
-        Set<String> authorities = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+    public ModelAndView index(Authentication authentication) {
+
         if (authentication.isAuthenticated()) {
-            if (authorities.contains("Sender")) {
+            String role = authentication.getAuthorities().iterator().next().getAuthority();
+            if (role.equals("Sender")) {
                 ModelAndView mv = new ModelAndView("sender/sender_page");
                 return mv;
             } else {
@@ -52,6 +44,7 @@ public class MainController {
 
     @GetMapping("/sender_my_offers")
     public String senderMyOffers() {
+
         return "sender/sender_my_offers";
     }
 }
