0f9955d6cd97edab285a9c202956e81c9ce25b5f
alls23 <alls232323@gmail.com>
2019-08-24 18:01:34 +0200

    Merge remote-tracking branch 'origin/dev' into user_edit
    # Conflicts:
    #	src/main/java/org.bootcamp.trashhunter/controllers/AvatarLoadController.java
    #	src/main/java/org.bootcamp.trashhunter/controllers/EditUserController.java
    #	src/main/java/org.bootcamp.trashhunter/controllers/RegistrationController.java
    #	src/main/java/org.bootcamp.trashhunter/controllers/UserRestController.java
    #	src/main/java/org.bootcamp.trashhunter/controllers/rest/ChangePasswordUserRestController.java
    #	src/main/java/org.bootcamp.trashhunter/controllers/rest/UserRestController.java
    #	src/main/java/org.bootcamp.trashhunter/dao/abstraction/BaseTokenDAO.java
    #	src/main/java/org.bootcamp.trashhunter/models/User.java
    #	src/main/java/org.bootcamp.trashhunter/services/AbstractServiceImpl.java
    #	src/main/resources/templates/sender/sender_edit_user.html

--- src/main/java/org.bootcamp.trashhunter/dao/impl/token/BaseTokenDAOImpl.java
+++ src/main/java/org.bootcamp.trashhunter/dao/impl/token/BaseTokenDAOImpl.java
@@ -1,34 +1,34 @@
-package org.bootcamp.trashhunter.dao.impl.token;
-
-import org.bootcamp.trashhunter.dao.impl.AbstractDAOImpl;
-import org.bootcamp.trashhunter.dao.impl.abstraction.BaseTokenDAO;
-import org.bootcamp.trashhunter.models.token.BaseToken;
-
-public class BaseTokenDAOImpl<T extends BaseToken> extends AbstractDAOImpl<T> implements BaseTokenDAO<T> {
-
-    @Override
-    public T findByToken(String token) {
-        return entityManager
-                .createQuery("SELECT t FROM " + clazz.getName() + " t WHERE t.token = :param", clazz)
-                .setParameter("param", token)
-                .getResultList()
-                .stream().findFirst().orElse(null);
-    }
-
-    @Override
-    public boolean existsTokenByUserId(Long userId) {
-        return !entityManager
-                .createQuery("SELECT t FROM " + clazz.getName() + " t WHERE t.user.id = :id", clazz)
-                .setParameter("id", userId)
-                .getResultList().isEmpty();
-    }
-
-    @Override
-    public T findTokenByUserId(Long userId) {
-        return entityManager
-                .createQuery("SELECT t FROM " + clazz.getName() + " t WHERE t.user.id = :id", clazz)
-                .setParameter("id", userId)
-                .getResultList()
-                .stream().findFirst().orElse(null);
-    }
-}
+//package org.bootcamp.trashhunter.dao.impl.token;
+//
+//import org.bootcamp.trashhunter.dao.impl.AbstractDAOImpl;
+//import org.bootcamp.trashhunter.dao.impl.abstraction.BaseTokenDAO;
+//import org.bootcamp.trashhunter.models.token.BaseToken;
+//
+//public class BaseTokenDAOImpl<T extends BaseToken> extends AbstractDAOImpl<T> implements BaseTokenDAO<T> {
+//
+//    @Override
+//    public T findByToken(String token) {
+//        return entityManager
+//                .createQuery("SELECT t FROM " + clazz.getName() + " t WHERE t.token = :param", clazz)
+//                .setParameter("param", token)
+//                .getResultList()
+//                .stream().findFirst().orElse(null);
+//    }
+//
+//    @Override
+//    public boolean existsTokenByUserId(Long userId) {
+//        return !entityManager
+//                .createQuery("SELECT t FROM " + clazz.getName() + " t WHERE t.user.id = :id", clazz)
+//                .setParameter("id", userId)
+//                .getResultList().isEmpty();
+//    }
+//
+//    @Override
+//    public T findTokenByUserId(Long userId) {
+//        return entityManager
+//                .createQuery("SELECT t FROM " + clazz.getName() + " t WHERE t.user.id = :id", clazz)
+//                .setParameter("id", userId)
+//                .getResultList()
+//                .stream().findFirst().orElse(null);
+//    }
+//}
--- src/main/java/org.bootcamp.trashhunter/services/impl/tokens/VerificationTokenService.java
+++ src/main/java/org.bootcamp.trashhunter/services/impl/tokens/VerificationTokenService.java
@@ -1,22 +1,22 @@
-package org.bootcamp.trashhunter.services.impl.tokens;
-
-import org.bootcamp.trashhunter.dao.impl.abstraction.VerificationTokenDAO;
-import org.bootcamp.trashhunter.models.token.VerificationToken;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-@Transactional
-public class VerificationTokenService extends BaseTokenService<VerificationToken> {
-
-    @Autowired
-    public VerificationTokenService(VerificationTokenDAO verificationTokenDAO) {
-        super(verificationTokenDAO);
-    }
-
-    public void completeRegistration(VerificationToken token) {
-        token.getUser().setEnabled(true);
-        abstractDAOImpl.delete(token);
-    }
-}
+//package org.bootcamp.trashhunter.services.impl.tokens;
+//
+//import org.bootcamp.trashhunter.dao.impl.abstraction.VerificationTokenDAO;
+//import org.bootcamp.trashhunter.models.token.VerificationToken;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@Service
+//@Transactional
+//public class VerificationTokenService extends BaseTokenService<VerificationToken> {
+//
+//    @Autowired
+//    public VerificationTokenService(VerificationTokenDAO verificationTokenDAO) {
+//        super(verificationTokenDAO);
+//    }
+//
+//    public void completeRegistration(VerificationToken token) {
+//        token.getUser().setEnabled(true);
+//        abstractDAOImpl.delete(token);
+//    }
+//}
