ea00246bffd08a5e140e628c8717c6eb06abfa56
ZuiQuanZ <123casper@mail.ru>
2019-08-27 14:22:55 +0300

    add login.js

--- src/main/java/org.bootcamp.trashhunter/TrashHunterApplication.java
+++ src/main/java/org.bootcamp.trashhunter/TrashHunterApplication.java
@@ -4,7 +4,6 @@ import org.bootcamp.trashhunter.config.InitData;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.annotation.PostConstruct;
 
