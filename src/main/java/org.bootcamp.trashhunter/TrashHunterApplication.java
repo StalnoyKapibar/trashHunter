bd650214aa42b817ed546d8e190da0fcd087deae
johnlivingstonseagul <johnlivingstonseagul@yandex.ru>
2019-08-30 17:29:27 +0400

    ready to merge into dev

--- src/main/java/org.bootcamp.trashhunter/TrashHunterApplication.java
+++ src/main/java/org.bootcamp.trashhunter/TrashHunterApplication.java
@@ -14,12 +14,11 @@ public class TrashHunterApplication {
     }
 
 //    нужно раскоментить @Bean для создания таблиц
-
     @Bean(initMethod = "init")
     @PostConstruct
+
     public InitData initData() {
         return new InitData();
     }
-
 
 }
--- src/main/java/org.bootcamp.trashhunter/config/InitData.java
+++ src/main/java/org.bootcamp.trashhunter/config/InitData.java
@@ -78,28 +78,32 @@ public class InitData {
         Statistics s4 = new Statistics(300, 2000, 2203, 1211);
         Sender sender1 = new Sender("sender1@mail.ru", "Михаил А.", "sender1", LocalDate.now(), "Viborg, Russia", userService.extractBytesDefaultAvatar("mixa.jpg"));
         sender1.setAddress(faker.address().streetName());
-        sender1.setAboutUser(faker.hipster().word());
+        sender1.setAboutUser(faker.lebowski().quote());
         sender1.setStatistics(s1);
         senderService.add(sender1);
         Sender sender2 = new Sender("sender2@mail.ru", "Максим В.", "sender2", LocalDate.now(), "Viborg, Russia", userService.extractBytesDefaultAvatar("max.jpg") );
         sender2.setAddress(faker.address().streetName());
-        Sender sender2 = new Sender("sender2@mail.ru", "Максим В.", "sender2", LocalDate.now(), "Viborg, Russia", userService.extractBytesDefaultAvatar("max.jpg"));
+        sender2.setAboutUser(faker.lebowski().quote());
         sender2.setStatistics(s2);
         senderService.add(sender2);
         Sender sender3 = new Sender("sender3@mail.ru", "Иван Ф.", "sender3", LocalDate.now(), "Viborg, Russia", userService.extractBytesDefaultAvatar("ivan.jpg"));
         sender3.setAddress(faker.address().streetName());
+        sender3.setAboutUser(faker.lebowski().quote());
         sender3.setStatistics(s3);
         senderService.add(sender3);
         Sender sender4 = new Sender("sender4@mail.ru", "Юрий П.", "sender4", LocalDate.now(), "Viborg, Russia", userService.extractBytesDefaultAvatar("iura.jpg"));
         sender4.setAddress(faker.address().streetName());
         sender4.setStatistics(s4);
+        sender4.setAboutUser(faker.lebowski().quote());
         senderService.add(sender4);
         for (int i = 0; i < 15; i++) {
-            Sender sender = new Sender("send" + i + "mail.ru",faker.name().firstName() + " " + faker.name().lastName(),
+            Sender sender = new Sender("send" + i + "@mail.ru",faker.name().firstName() + " " + faker.name().lastName(),
                     "sender" + i, LocalDate.now(),"Viborg, Russia",
                     userService.extractBytesDefaultAvatar("l" + i + ".jpg"));
                     sender.setAddress(faker.address().streetAddress());
                     sender.setAboutUser(faker.lebowski().quote());
+                    Statistics s = new Statistics(10, 50, 12, 240);
+                    sender.setStatistics(s);
                     senderService.add(sender);
         }
     }
@@ -111,26 +115,31 @@ public class InitData {
 
         Taker taker1 = new Taker("taker1@mail.ru", "Юра З.", "taker1", LocalDate.now(), "Viborg, Russia", userService.extractBytesDefaultAvatar("yura.jpg"));
         taker1.setAddress(faker.address().streetName());
+        taker1.setAboutUser(faker.lebowski().quote());
         taker1.setStatistics(s1);
         takerService.add(taker1);
         Taker taker2 = new Taker("taker2@mail.ru", "Матвей О.", "taker2", LocalDate.now(), "Viborg, Russia", userService.extractBytesDefaultAvatar("matey.jpg"));
         taker2.setAddress(faker.address().streetName());
+        taker2.setAboutUser(faker.lebowski().quote());
+        taker2.setAboutUser(faker.lebowski().quote());
         taker2.setStatistics(s2);
         takerService.add(taker2);
         Taker taker3 = new Taker("taker3@mail.ru", "Денис Т.", "taker3", LocalDate.now(), "Viborg, Russia", userService.extractBytesDefaultAvatar("denis.jpg"));
         taker3.setAddress(faker.address().streetName());
+        taker3.setAboutUser(faker.lebowski().quote());
         taker3.setStatistics(s3);
         takerService.add(taker3);
         for (int i = 0; i < 15; i++) {
-            Taker taker = new Taker("take" + i + "mail.ru",faker.name().firstName() + " " + faker.name().lastName(),
+            Taker taker = new Taker("take" + i + "@mail.ru",faker.name().firstName() + " " + faker.name().lastName(),
                                     "taker" + i, LocalDate.now(),"Viborg, Russia",
                                     userService.extractBytesDefaultAvatar(i+".jpg"));
-
+              taker.setAboutUser(faker.lebowski().quote());
               taker.setAddress(faker.address().streetName());
+              Statistics s = new Statistics(10, 50, 12, 240);
+              taker.setStatistics(s);
               takerService.add(taker);
         }
-//        Taker taker4 = new Taker("taker4@mail.ru", "Владислав Ы.", "taker4", LocalDate.now(), "Viborg, Russia", userService.extractBytesDefaultAvatar("vlad.png"));
-//        takerService.add(taker4);
+
     }
 
     private void initRandomOffers(int quantity) {
@@ -187,7 +196,7 @@ public class InitData {
                 List<Taker> allTakers = takerService.getAll();
                 Collections.shuffle(allTakers);
                 List<Taker> takersForRandomOffer = new ArrayList<>();
-                for (int j = 0; j < 6; j++) {
+                for (int j = 0; j < (int)(Math.random()*10 + 2); j++) {
                     takersForRandomOffer.add(allTakers.get(j));
 
                 }
@@ -195,12 +204,12 @@ public class InitData {
             }
             if (randomOffer.getOfferStatus().equals(OfferStatus.TAKEN)) {
                 List<Taker> takers = new ArrayList<>();
-                takers.add(takerService.getById(5L));
+                takers.add(takerService.getById(20L));
                 randomOffer.setRespondingTakers(takers);
             }
             if (randomOffer.getOfferStatus().equals(OfferStatus.COMPLETE)) {
                 List<Taker> takers = new ArrayList<>();
-                takers.add(takerService.getById(6L));
+                takers.add(takerService.getById(21L));
                 randomOffer.setRespondingTakers(takers);
             }
             offerService.add(randomOffer);
